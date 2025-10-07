d658f236d958c55d07da712da8d46898b88b9859
David Villa Alises <david.villa@gmail.com>
2025-10-07 17:24:30 +0200

    don't show stats

--- try_player.py
+++ try_player.py
@@ -5,12 +5,12 @@ import time
 from gst_player import GstPlayer
 
 player = GstPlayer()
-player.show_stats = True
 player.start()
 
 with open('test/media/4s.mp3', 'rb') as fd:
     player.configure(fd.read)
     player.confirm_play_starts()
+
     while player.is_playing():
         time.sleep(0.5)
 
